71981d5507ec842e0d2386cb29778db28d5ab3cd
1069163331@qq.com <1069163331@qq.com>
2018-06-21 13:24:21 +0800

    trival

--- validator/dataset_validator.py
+++ validator/dataset_validator.py
@@ -11,95 +11,34 @@ sys.path.append('../')
 
 import tensorflow as tf
 import tensorflow.contrib.layers as tcl
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import tensorflow.contrib.layers as tcl
-=======
-import tensorflow.contrib.layer as tcl
->>>>>>> 2282cc26ef1f45414cb1b313771973457884af17
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
-=======
->>>>>>> 55376a86b9c35c0f7edd76837b1349031a23a501
-
 
 from utils.metric import get_metric
 
 class DatasetValidator(object):
 	def __init__(self, config):
-		self.config = config
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-		self.config = config
-=======
-
 
 		self.config = config
-
->>>>>>> 2282cc26ef1f45414cb1b313771973457884af17
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
-=======
->>>>>>> 55376a86b9c35c0f7edd76837b1349031a23a501
 		self.nb_samples = config.get('num_samples', 30)
 		self.metric = config.get('metric', 'accuracy')
 		self.metric_type = config.get('metric type', 'top1')
+		self.assets_dir = config['assets dir']
 
-		self.assets_dir = config['assets dir']
-=======
-<<<<<<< HEAD
-		self.assets_dir = config['assets dir']
 
 
 	def build_summary(self, model):
 		if self.metric == 'accuracy':
-<<<<<<< HEAD
-=======
-		# self.test_indices = []
 
-		# summary_list = []
-		# summary_list.append(
-
-		# )
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
-
-
-	def build_summary(self, model):
-		if self.metric == 'accuracy':
-<<<<<<< HEAD
-=======
-
->>>>>>> 2282cc26ef1f45414cb1b313771973457884af17
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
-=======
->>>>>>> 55376a86b9c35c0f7edd76837b1349031a23a501
 			self.label = tf.placeholder(tf.float32, shape=[None, model.nb_classes],
 							name='test_label')
 			self.predict = tf.placeholder(tf.float32, shape=[None, model.nb_classes],
 							name='test_predict')
 			self.accuracy = get_metric(self.metric, self.metric_type, 
 						{'probs' : self.predict, 'labels' : self.label, 'decay' : 1})			
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-			self.accuracy = get_metric(self.metric, self.metric_type, 
-						{'probs' : self.predict, 'labels' : self.label, 'decay' : 1})			
-=======
-
-			self.accuracy = get_metric(self.metric, self.metric_type, 
-						{'probs' : self.predict, 'label' : self.label, 'decay' : 1})			
->>>>>>> 2282cc26ef1f45414cb1b313771973457884af17
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
-=======
->>>>>>> 55376a86b9c35c0f7edd76837b1349031a23a501
 
 			self.summary_list = []
 			self.summary_list.append(tf.summary.scalar('test acc ' + self.metric_type, self.accuracy))
 
 			self.log_filepath = os.path.join(self.assets_dir, 'test_dataset_' + self.metric + "_" + self.metric_type + '.csv')
-=======
-<<<<<<< HEAD
-			self.log_filepath = os.path.join(self.assets_dir, 'test_dataset_' + self.metric + "_" + self.metric_type + '.csv')
 
 			with open(self.log_filepath, 'w') as logfile:
 				logfile.write('step,'+self.metric_type+'\n')
@@ -107,25 +46,6 @@ class DatasetValidator(object):
 		self.summary = tf.summary.merge(self.summary_list)
 
 
-	def validate(self, model, dataset, sess, step):
-		label_list = []
-		pred_list = []
-		for ind, batch_x, batch_y in dataset.iter_test_images():
-			pred_y = model.predict(sess, batch_x)
-			label_list.append(batch_y)
-			pred_list.append(pred_y)
-		label_list = np.concatenate(label_list, axis=0)
-		pred_list = np.concatenate(pred_list, axis=0)
-
-<<<<<<< HEAD
-=======
-		self.summary = tf.summary.merge(self.summary_list)
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
-
-			with open(self.log_filepath, 'w') as logfile:
-				logfile.write('step,'+self.metric_type+'\n')
-
-		self.summary = tf.summary.merge(self.summary_list)
 
 
 	def validate(self, model, dataset, sess, step):
@@ -139,36 +59,18 @@ class DatasetValidator(object):
 			pred_list.append(pred_y)
 		label_list = np.concatenate(label_list, axis=0)
 		pred_list = np.concatenate(pred_list, axis=0)
-<<<<<<< HEAD
 
-=======
-		
->>>>>>> 2282cc26ef1f45414cb1b313771973457884af17
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
-
-=======
->>>>>>> 55376a86b9c35c0f7edd76837b1349031a23a501
 		if self.metric == 'accuracy' : 
 			feed_dict = {
 				self.label : label_list,
 				self.predict : pred_list,
 			}
-			acc, summary = sess.run([self.accuracy, self.summary], feed_dict=feed_dict)
 
-			with open(self.log_filepath, 'a') as logfile:
-				logfile.write('%d,%f\n'%(step, acc))
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 			acc, summary = sess.run([self.accuracy, self.summary], feed_dict=feed_dict)
 			with open(self.log_filepath, 'a') as logfile:
 				logfile.write('%d,%f\n'%(step, acc))
-=======
+
 			summary = sess.run([self.summary], feed_dict=feed_dict)[0]
->>>>>>> 2282cc26ef1f45414cb1b313771973457884af17
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
-=======
->>>>>>> 55376a86b9c35c0f7edd76837b1349031a23a501
 
 		return summary
 
--- validator/validator.py
+++ validator/validator.py
@@ -23,10 +23,6 @@
 # ==============================================================================
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 99e16f29cc5f3bb2fbfc86a30f95e2bea1955734
 def get_validator(name, config):
     
     if name == 'hidden_variable_validator':
